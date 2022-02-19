418a00e7efbb1beb2b349ecf98a61654f741f87b
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-02-19 09:38:31 -0800

    Add expire index in secrets collection

--- db/config.py
+++ db/config.py
@@ -4,3 +4,4 @@ from pymongo.collection import Collection
 client = motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
 db = client.test_database
 secrets: Collection = db.secrets
+secrets.create_index("created", expireAfterSeconds=5)  # By default delete a document after 1 day
