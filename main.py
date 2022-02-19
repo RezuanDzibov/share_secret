cdfd44c24ff6a297e4155eefa343d4de7544e5c8
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-02-19 07:54:54 -0800

    Move doctsrings

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = FastAPI()
 
 
 @app.post("/secret")
+async def create_secret(schema: SecretCreate):
     """
     Create a secret.
     
@@ -14,13 +15,13 @@ app = FastAPI()
     :type schema: SecretCreate
     :return: The ID of the inserted secret.
     """
-async def create_secret(schema: SecretCreate):
     secret: dict = schema.dict()
     result = await secrets.insert_one(secret)
     return str(result.inserted_id)
 
 
 @app.post("/secret/{_id}", response_model=SecretRetrive)
+async def get_secret(_id: PyObjectId, key: str = Form(...)):
     """
     Retrive a secret from the database
     
@@ -30,7 +31,6 @@ async def create_secret(schema: SecretCreate):
     :type key: str
     :return: The secret is being returned.
     """
-async def get_secret(_id: PyObjectId, key: str = Form(...)):
     secret = await secrets.find_one({"_id": _id, "key": key})
     if secret is None:
         raise HTTPException(status_code=404, detail=f"There isn't any secrets with id({_id}) and key({key}).")
