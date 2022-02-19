51991a833f61370f9ae606d0d5f802c6468f47a4
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-02-19 06:29:50 -0800

    Add PyObjectId, SecretRetreive schems

--- schemas.py
+++ schemas.py
@@ -1,6 +1,34 @@
-from pydantic import BaseModel
+from bson import ObjectId
+from pydantic import BaseModel, Field
 
 
 class SecretCreate(BaseModel):
     key: str
     text: str
+
+    class Config:
+        allow_mutation = True
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class SecretRetrive(SecretCreate):
+    id_: PyObjectId() = Field(alias="_id")
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
