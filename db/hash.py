9369fe832c9805b0390faf13195044b0fba6f67a
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-02-19 08:38:02 -0800

    Refactor hash

--- db/hash.py
+++ db/hash.py
@@ -1,13 +1,12 @@
 import bcrypt
 
 
-async def hash_secret_key(key: str) -> str:
+async def hash_secret_key(key: str) -> bytes:
     key = bytes(key, "utf-8")
     hashed_key = bcrypt.hashpw(key, bcrypt.gensalt())
-    return str(hashed_key)
+    return hashed_key
 
 
 async def is_valid_secret_key(raw_key: str, hashed_key) -> bool:
     raw_key = bytes(raw_key, "utf-8")
-    hashed_key = bytes(hashed_key, "utf-8")
     return bcrypt.checkpw(raw_key, hashed_key)
