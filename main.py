27e372df52d6c9f332f66a0fd9a45e04d34dc2ec
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-02-19 06:58:51 -0800

    Add doctsrings

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, HTTPException
 
 from db.config import secrets
-from schemas import SecretCreate, SecretRetrive, PyObjectId
+from schemas import PyObjectId, SecretCreate, SecretRetrive
 
 app = FastAPI()
 
 
 @app.post("/secret")
+    """
+    Create a secret.
+    
+    :param schema: SecretCreate is a parameter that takes in a SecretCreate object
+    :type schema: SecretCreate
+    :return: The ID of the inserted secret.
+    """
 async def create_secret(schema: SecretCreate):
     secret: dict = schema.dict()
     result = await secrets.insert_one(secret)
     return str(result.inserted_id)
 
 
-@app.get("/secret/{_id}", response_model=SecretRetrive)
-async def get_secret(_id: PyObjectId):
-    secret = await secrets.find_one({"_id": _id})
+@app.post("/secret/{_id}", response_model=SecretRetrive)
+    """
+    Retrive a secret from the database
+    
+    :param _id: PyObjectId
+    :type _id: PyObjectId
+    :param key: str = Form(...)
+    :type key: str
+    :return: The secret is being returned.
+    """
+async def get_secret(_id: PyObjectId, key: str = Form(...)):
+    secret = await secrets.find_one({"_id": _id, "key": key})
+    if secret is None:
+        raise HTTPException(status_code=404, detail=f"There isn't any secrets with id({_id}) and key({key}).")
+    await secrets.delete_one({"_id": _id})
     return secret
