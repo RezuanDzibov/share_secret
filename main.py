8f9eb7b4a145a4115a6d2d4995305d55e754ef5c
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-02-19 06:30:53 -0800

    Add create_secret, get_secret endpoints

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 from fastapi import FastAPI
 
+from db.config import secrets
+from schemas import SecretCreate, SecretRetrive, PyObjectId
+
 app = FastAPI()
+
+
+@app.post("/secret")
+async def create_secret(schema: SecretCreate):
+    secret: dict = schema.dict()
+    result = await secrets.insert_one(secret)
+    return str(result.inserted_id)
+
+
+@app.get("/secret/{_id}", response_model=SecretRetrive)
+async def get_secret(_id: PyObjectId):
+    secret = await secrets.find_one({"_id": _id})
+    return secret
