5c9a331c4861f5a1659dfba8bac075499c3d5445
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-02-20 02:53:09 -0800

    Add import mongo url from env

--- db/config.py
+++ db/config.py
@@ -1,7 +1,14 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
 from motor import motor_asyncio
 from pymongo.collection import Collection
 
-client = motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
+BASE_DIR = Path(__file__).parent.parent
+load_dotenv(dotenv_path=BASE_DIR / ".env")
+
+client = motor_asyncio.AsyncIOMotorClient(os.getenv("MONGO_URL"))
 db = client.database
 secrets: Collection = db.secrets
 secrets.create_index("created", expireAfterSeconds=86400)  # By default delete a document after 1 day
